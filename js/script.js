707bc824c46e61aa5117725c42e3911c228bf94c
Ivan POMAHOB <3gopobo@gmail.com>
2024-04-17 12:43:10 +0000

    add buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $('p').click(function(){
-    $('this').slideToggle('slow');
+    $(this).slideToggle('slow');
+});
+$('p').mouseenter(function(){
+    $('p').css("color", "red");
+
+});
+$('p').mouseleave(function(){
+    $('p').css("color", "black");
+});
+$('button').mouseenter(function(){
+    $(this).removeClass('makeRed').addClass('makeBlue';)
+});
+$('button').mouseleave(function(){
+    $('this').removeClass('makeBlue').addClass('makeRed');
 });
