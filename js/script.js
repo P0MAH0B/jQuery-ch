38bd9077087f64c4635a9d74fe212f4c42ff5331
Ivan POMAHOB <3gopobo@gmail.com>
2024-04-17 13:31:18 +0000

    add click event

--- js/script.js
+++ js/script.js
@@ -9,8 +9,19 @@ $('p').mouseleave(function(){
     $('p').css("color", "black");
 });
 $('button').mouseenter(function(){
-    $(this).removeClass('makeRed').addClass('makeBlue';)
+    $(this).removeClass('makeRed').addClass('makeBlue');
 });
 $('button').mouseleave(function(){
-    $('this').removeClass('makeBlue').addClass('makeRed');
+    $(this).removeClass('makeBlue').addClass('makeRed');
+});
+$(".box").click(function(){
+    var classNames = $(this).attr('class').split(" ");
+    var boxClass = classNames[0];
+    var className = classNames[1];
+    if ($(this).css("background-color") == "rgb(255,0,0)"){
+        $("." + className).css("background-color", "#000");
+    } else {
+       $("." + boxClass).css("background-color","#000");
+       $("." + className).css("background-color","red"); 
+    }
 });
